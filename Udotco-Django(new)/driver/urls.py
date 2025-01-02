3b9b3f96ff0d7f43606ec92b1f936cd253a936e1
Harley Dave Chavez <127485543+HATDOGnamayICE@users.noreply.github.com>
2025-01-02 23:20:24 +0800

    Second commit

--- Udotco-Django(new)/driver/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'driver'
-urlpatterns = [
-    path('', views.driver_request, name='driver_request'),
-    path('add_request/', views.add_request, name='add_request'),
-    path('logout/', views.logout_view, name='logout'),
-]
